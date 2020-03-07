b7db2e5fcae534514e8654880b9cbbdd9e89a409
Jimmy Lin <jimmylin@Lillians-MacBook-Pro-2018.local>
2020-03-06 23:44:03 -0800

    added for user disconnect

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ io.on('connection', socket => {
     //sends down to all other connected clients other than sender
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
   })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  })
 })
 
